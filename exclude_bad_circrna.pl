0b6e0210623b3b95fdf9fbe03b4f8cf487a69133
Kai WANG <49387752+ngsomics@users.noreply.github.com>
2021-02-09 19:18:43 +0800

    Add files via upload

---
+++ exclude_bad_circrna.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use List::Util qw/max min sum maxstr minstr shuffle/;
+
+
+die "Usage: perl $0 CIRC.sam CIRC.bed > CIRC.bed.good\n" unless (@ARGV == 2);
+
+my %hash = ();
+my @array = ();
+my $cov_n = 10;
+my $cov_p = 0.8;
+
+open (IN, "$ARGV[0]") || die "cannot open $ARGV[0]\n";
+while(<IN>){
+	chomp;
+	#E00603:107:HLKG3CCXY:2:1202:16193:48634_A__GATGGAACTGGTGGGCAACCAGAGTGGACCTTACAGAGGGTGACAATGGAAAACGTGAAGAGCAAGAAGACCCTACATTTTGTTGCAAATGTGTGTCTTT 16      chr1    4153955 32 20M      *       0       0       GGTTGCCCACCAGTTCCATC    iiiiiiiiiiiiiiiee```    AS:i:0  XS:i:-6 XN:i:0  XM:i:0  XO:i:0  XG:i:0  NM:i:0  MD:Z:20 YT:Z:UU
+	#bowtie2
+	#QNAME, FLAG, RNAME, POS, MAPQ, CIGAR, RNEXT, PNEXT, TLEN, SEQ, QUAL
+	
+	#https://blog.csdn.net/weixin_42670653/article/details/109185212
+	#https://www.cnblogs.com/yahengwang/p/10695981.html
+	#1： 是paired-end或者mate pair的一条
+	#2： 双末端比对的一条
+	#4： 没有对比上参考序列
+	#8：是paired-end或者mate pair的一条，且无法比对上参考序列上
+	#16：比对到参考序列的负链上
+	#32： 双末端reads的另一条比对到参考序列的负链上
+	#64：这条reads是mate1
+	#128： 这条reads是mate2
+	
+	#	比对区11个列和可选列的解释
+	#1  QNAME  比对的序列名
+	#2  FLAG   Bwise FLAG(表明比对类型：pairing，strand，mate strand等)
+	#3  RNAME  比对上的参考序列名
+	#4  POS    1-Based的比对上的最左边的定位
+	#5  MAPQ   比对质量
+	#6  CIGAR  Extended CIGAR string (操作符：MIDNSHP) 比对结果信息：匹配碱基数，可变剪接等。
+	#7  MRNM   相匹配的另外一条序列，比对上的参考序列名
+	#8  MPOS   1-Based leftmost Mate POsition
+	#9  ISIZE  插入片段长度
+	#10 SEQ    和参考序列在同一个琏上的比对序列(若比对结果在负意链上，则序列是其反向重复序列)
+	#11 QUAL   比对序列的质量(ASCII-33=Phred base quality)
+	#12 可选的行，以TAG：TYPE：VALUE的形式提供额外的信息
+
+	#SN1077:321:H2NH5BCXX:1:1103:1329:2233   99      S51     126036  60      133M    =       126036  -133    AAACCTTCGGGGCCTTGACTAAATTATCTTCTACGATCAAAATATCGTGGACTTCTGTCAGAATGTCCATCATATGGCCTGCGTTCATGAAAGAACTCTCTTGGAGGTGGATATCCGCCTGGCAAAGGAGAAT      DDDDDIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIHIIIIIIIIIHIIIIIIIIIIIIIHIIIIHIIIIIIIIIIGHIIIIIIIIIIIIIHIHFDHHIIIIIHHIIIIIIIIIHIIIHIIIIHII      AS:i:-12        XN:i:0  XM:i:2  XO:i:0  XG:i:0  NM:i:2  MD:Z:13T10G108  YS:i:-12        YT:Z:CP NH:i:1
+	#SN1077:321:H2NH5BCXX:1:1103:1329:2233   147     S51     126036  60      133M    =       126036  -133    AAACCTTCGGGGCCTTGACTAAATTATCTTCTACGATCAAAATATCGTGGACTTCTGTCAGAATGTCCATCATATGGCCTGCGTTCATGAAAGAACTCTCTTGGAGGTGGATATCCGCCTGGCAAAGGAGAAT      HHFHHIHIIIIIIIIIIIIIIIIIIIIIIIHFIIIIIIIIIIHHIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIHIHGIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIDDDDD      AS:i:-12        XN:i:0  XM:i:2  XO:i:0  XG:i:0  NM:i:2  MD:Z:13T10G108  YS:i:-12        YT:Z:CP NH:i:1
+
+	next if (/^\@HD\s+/);
+	next if (/^\@SQ\s+/);
+	next if (/^\@PG\s+/);
+	
+	@array = ();
+	@array = split(/\t/, $_);
+	my $flag       = $array[1];
+	my $chromosome = $array[2];
+	next if ($chromosome eq "*");
+	
+	my $c_s        = $array[7];
+	my $insert_size = $array[8]; #bowtie2
+	next if ($insert_size < 0);
+	
+	my $c_e = $c_s + $insert_size;
+	
+	#my $strand = "+";
+	#if ($flag == 0){
+	#	$strand = "-";
+	#}
+	#elsif ($flag == 16){
+	#	$strand = "-";
+	#}
+	
+	for (my $i = $c_s; $i <= $c_e; $i ++){
+		unless (exists $hash{$chromosome}{$i}){
+			$hash{$chromosome}{$i} = 0;
+		}
+		else{
+			$hash{$chromosome}{$i} ++;
+		}
+	}
+}
+close(IN);
+
+my $cnt = 0;
+open (IN, "$ARGV[1]") || die "cannot open $ARGV[1]\n";
+while(<IN>){
+	chomp;
+	#chromosome      circ_start      circ_end        circrna_id      length  strand
+	#chr1    10035157        10037607        chr1:10035157-10037607:+|CIRC_000001    2.451   +
+	
+	my @array = split (/\t/, $_);
+	my $chromosome = $array[0];
+	my $circrna_s  = $array[1];
+	my $circrna_e  = $array[2];
+	my $strand     = $array[5];
+	next unless ($circrna_s =~ /\d+/);
+	
+	my $c_num = 0;
+	for (my $i = $circrna_s; $i <= $circrna_e; $i ++){
+		if ($hash{$chromosome}{$i} >= $cov_n){
+			$c_num ++;
+		}
+	}
+	
+	if ($c_num / ($circrna_e - $circrna_s + 1) >= $cov_p){
+		$cnt ++;
+		my $lens = ($circrna_e - $circrna_s + 1) / 1000;
+		my $circrna_id = "$chromosome\:$circrna_s\-$circrna_e\:$strand";
+		my $CIRCID = sprintf ("%06d", $cnt);
+		print "$chromosome\t$circrna_s\t$circrna_e\t$circrna_id\|CIRC\_$CIRCID\t$lens\t$strand\n";
+	}
+}
+close(IN);
--- selct_circrna.pl
+++ selct_circrna.pl
@@ -18,7 +18,7 @@ while(<IN>){
 	#circRNA_ID      chr     circRNA_start   circRNA_end     #junction_reads SM_MS_SMS       #non_junction_reads     junction_reads_ratio circRNA_type    gene_id strand  junction_reads_ID
 	my @array = split (/\t/, $_);
 	#my $circrna_id = $array[0];
-	my $chromesome = $array[1];
+	my $chromosome = $array[1];
 	my $circrna_s  = $array[2];
 	my $circrna_e  = $array[3];
 	my $strand     = $array[10];
@@ -26,7 +26,7 @@ while(<IN>){
 	next if ($circrna_e - $circrna_s + 1 > $lens_cut);
 	
 	my $circ = "$circrna_s\-$circrna_e";
-	push (@{$hash{$chromesome}{$strand}}, [$circrna_s, $circrna_e]);
+	push (@{$hash{$chromosome}{$strand}}, [$circrna_s, $circrna_e]);
 	$cnt ++;
 }
 close(IN);
@@ -39,7 +39,7 @@ while(<IN>){
 	chomp;
 	# chrom start   end     name    n_reads strand  n_uniq  uniq_bridges    best_qual_left  best_qual_right tissues tiss_counts  edits   anchor_overlap  breakpoints     signal  strandmatch     category
 	my @array = split (/\t/, $_);
-	my $chromesome = $array[0];
+	my $chromosome = $array[0];
 	my $circrna_s  = $array[1];
 	my $circrna_e  = $array[2];
 	my $strand     = $array[5];
@@ -57,15 +57,15 @@ $cnt = 0;
 
 sub find_overlapping {
 	my ($c_s, $c_e) = @_;
-	foreach my $chromesome (keys %hash) {
-		foreach my $strand (keys %{$hash{$chromesome}}) {
-			foreach my $e (sort {$$a[0] <=> $$b[0]} @{$hash{$chromesome}{$strand}}) {
+	foreach my $chromosome (keys %hash) {
+		foreach my $strand (keys %{$hash{$chromosome}}) {
+			foreach my $e (sort {$$a[0] <=> $$b[0]} @{$hash{$chromosome}{$strand}}) {
 				my ($circrna_s, $circrna_e) = ($$e[0], $$e[1]);
 				
 				my $circ = "$circrna_s\-$circrna_e";
 				
 				if (abs($circrna_s - $c_s) <= $adjacent_distance && abs($circrna_e - $c_e) <= $adjacent_distance){
-					$circs{$chromesome}{$strand}{$circ} = 1;
+					$circs{$chromosome}{$strand}{$circ} = 1;
 				}
 			}
 		}
@@ -75,23 +75,23 @@ sub find_overlapping {
 sub find_unis {
 	my %uni = ();
 	$cnt = 0;
-	print "chromesome\tcirc_start\tcirc_end\tcircrna_id\tlength\tstrand\n";
-	foreach my $chromesome (sort keys %circs) {
-		foreach my $strand (sort keys %{$circs{$chromesome}}) {
-			foreach my $circ (sort keys %{$circs{$chromesome}{$strand}}) {
+	print "chromosome\tcirc_start\tcirc_end\tcircrna_id\tlength\tstrand\n";
+	foreach my $chromosome (sort keys %circs) {
+		foreach my $strand (sort keys %{$circs{$chromosome}}) {
+			foreach my $circ (sort keys %{$circs{$chromosome}{$strand}}) {
 				my ($circ_s, $circ_e) = $circ =~ /^(\d+)\-(\d+)$/;
-				my $circrna_id = "$chromesome\:$circ_s\-$circ_e\:$strand";
+				my $circrna_id = "$chromosome\:$circ_s\-$circ_e\:$strand";
 				my $lens = ($circ_e - $circ_s + 1) / 1000;
 				
-				unless (exists $uni{$chromesome}{$strand}){
-					$uni{$chromesome}{$strand}{$circ} = 1;
+				unless (exists $uni{$chromosome}{$strand}){
+					$uni{$chromosome}{$strand}{$circ} = 1;
 					$cnt ++;
 					my $CIRCID = sprintf ("%06d", $cnt);
-					print "$chromesome\t$circ_s\t$circ_e\t$circrna_id\|CIRC\_$CIRCID\t$lens\t$strand\n";
+					print "$chromosome\t$circ_s\t$circ_e\t$circrna_id\|CIRC\_$CIRCID\t$lens\t$strand\n";
 				}
 				else{
 					my $flag = 0;
-					foreach my $e (sort keys %{$uni{$chromesome}{$strand}}){
+					foreach my $e (sort keys %{$uni{$chromosome}{$strand}}){
 						next if ($e eq $circ);
 						
 						my ($c_s, $c_e) = $e =~ /^(\d+)\-(\d+)$/;
@@ -102,14 +102,14 @@ sub find_unis {
 					}
 					
 					if ($flag == 0){
-						$uni{$chromesome}{$strand}{$circ} = 1;
+						$uni{$chromosome}{$strand}{$circ} = 1;
 						$cnt ++;
 						my $CIRCID = sprintf ("%06d", $cnt);
-						print "$chromesome\t$circ_s\t$circ_e\t$circrna_id\|CIRC\_$CIRCID\t$lens\t$strand\n";
+						print "$chromosome\t$circ_s\t$circ_e\t$circrna_id\|CIRC\_$CIRCID\t$lens\t$strand\n";
 					}
 				}
 				
-				#print "$chromesome\t$circ_s\t$circ_e\t$circrna_id\t$lens\t$strand\n";
+				#print "$chromosome\t$circ_s\t$circ_e\t$circrna_id\t$lens\t$strand\n";
 				#sleep (1);
 			}
 		}
